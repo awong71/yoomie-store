f211b2d24a4e94be8a64c8bfbd1321025207b1b1
awong71 <albertjameswong@ymail.com>
2022-02-13 07:43:11 -0700

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-import { db } from "./libs/firebase/firebaseConfig";
-
+import {ref as dataRef, get, set, update} from 'firebase/database';
+import {db} from './libs/firebase/firebaseConfig';
+import {productCard} from './templates/productCard'
 // Find and assign your constant.
 
 
@@ -9,3 +10,34 @@ const iconToggle = document.querySelector('.menu-icon');
 iconToggle.addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('active-nav');
 });
+
+
+async function pageInit(){
+    const productRef = dataRef(db, 'products/');
+    const productSnapShot = await get(productRef)
+    const data = productSnapShot.val();
+   
+    // Object of Objects  rental{{},{},{}}
+    // Arrays of Objects
+    // map filter reduce sort find ....
+    // Object.keys()  Object.enteries() Object.values();
+
+    // const productCard =   Object.values(data).map(product=>{
+    //         const card = productCard(product)
+    //         // layout thrashing
+    //         document.body.append(card)
+    //         return card
+    //   })
+
+    Object.values(data).map(product=>{
+        const card = productCard(product)
+        document.querySelector('.card').append(card)
+    })
+      
+      
+
+
+    
+}
+
+pageInit()
--- src/js/templates/productCard.js
+++ src/js/templates/productCard.js
@@ -1,27 +1,48 @@
 import {ref as dataRef, get, set, update, remove} from 'firebase/database';
 import {db} from '../libs/firebase/firebaseConfig';
 function productCard ({key, productName, urlPath, productDetails, like, price}){
-  const template = `
+//   const template = `
       
-  <aside class="vacation-rentals">
+//   <aside>
 
-  <figure>
-  <img src="${urlPath}" width="160" alt="teether">
-      <figcaption> <h2>${productName}</h2></figcaption>
-      <figcaption> <h2>${productDetails}</h2></figcaption>
-      <figcaption> <h2>${like}</h2></figcaption>
-      <figcaption> <h2>${price}</h2></figcaption>
-  </figure>
+//   <figure>
+//   <img src="${urlPath}" width="160" alt="teether">
+//       <figcaption> <h2>${productName}</h2></figcaption>
+//       <figcaption> <h2>${productDetails}</h2></figcaption>
+//       <figcaption> <h2>${like}</h2></figcaption>
+//       <figcaption> <h2>${price}</h2></figcaption>
+//   </figure>
 
-  <footer>
-      <button id="edit" data-key="${key}" >edit</button>
-      <button id="delete" data-key="${key}" >delete</button>
-  </footer>
+//   <footer>
+//       <button id="edit" data-key="${key}" >edit</button>
+//       <button id="delete" data-key="${key}" >delete</button>
+//   </footer>
 
-</aside>
-  `
+// </aside>
+//   `
+
+    const template = `
+    <aside>
+    <div class="container">
+              <div class="card">
+                <div class="card-content">
+                    <img src="${urlPath}" alt="">
+                  <div class="product-details">
+                    <h3 class="card-header">${productName}</h3>
+                    <p class="card-info">${productDetails}</p>
+                    <p class="card-info">${like}</p>
+                    <p class="card-info">${price}</p>
+                    <button id="edit" data-key="${key}" class="card-button">Edit</button>
+                    <button id="delete" data-key="${key}" class="card-button">Delete</button>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </aside>
+    `
   const element = document.createRange().createContextualFragment(template).children[0]
   addCardControls(element)
+  console.log(element)
   return element
 }
 
